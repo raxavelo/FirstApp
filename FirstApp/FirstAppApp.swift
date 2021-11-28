29c84b86fa553631283a73987a3cab69064940e1
raxavelo <raxavelo@gmail.com>
2021-11-28 18:04:05 +0100

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by Grzegorz Zadkowski on 28/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
